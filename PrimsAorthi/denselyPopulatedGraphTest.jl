3fc3a6749ae46081e5e0492bb442bf0fd66012f8
Aorthi <aorthi@hotmail.com>
2019-06-01 16:25:38 +1200

    Testing with dense graphs

--- PrimsAorthi/denselyPopulatedGraphTest.jl
+++ PrimsAorthi/denselyPopulatedGraphTest.jl
@@ -1,72 +1,22 @@
 
-# Comment these package adds in and out as needed
-#import Pkg;
-#Pkg.add("GraphPlot")
-#Pkg.add("LightGraphs")
-#Pkg.add("LightGraphs")
-#Pkg.add("Compose")
-#Pkg.add("Fontconfig")
-#Pkg.add("Cairo")
-#Pkg.add("Distributed")
-#Pkg.add("BenchmarkTools")
-#Pkg.add("SimpleTraits")
-#Pkg.add("DataStructures")
-#Pkg.add("SimpleWeightedGraphs")
-#Pkg.add("Luxor")
+import Pkg;
+Pkg.add("LightGraphs")
+Pkg.add("SimpleWeightedGraphs")
+Pkg.add("BenchmarkTools")
 
-using GraphPlot
 using LightGraphs
-import LightGraphs
-import LightGraphs.Parallel
-#using Compose
-#using Fontconfig
-#using Cairo
-using Distributed
+using SimpleWeightedGraphs
 using BenchmarkTools
-using SimpleTraits
-import DataStructures
-import SimpleWeightedGraphs
-using Luxor
 
-# Make a graphWidth x graphWidth grid with random weights on edges from 1 : maxWeight
-function makeGrid(graphWidth::Int64, maxWeight::Float64)
-    vertexAmount = graphWidth ^ 2
-    graph = SimpleWeightedGraphs.SimpleWeightedGraph(vertexAmount)
-    for i::Int64 in 1 : vertexAmount
-        if (i % graphWidth > 0)
-            LightGraphs.add_edge!(graph, i, i + 1, rand(Float64) * maxWeight)
-        end
-        if ((i - 1) / graphWidth < graphWidth - 1)
-            LightGraphs.add_edge!(graph, i, i + graphWidth, rand(Float64) * maxWeight)
-        end
+# Create a weighted graph to test with
+function createTestGraph(graphSize)
+    G = SimpleWeightedGraph(graphSize);
+
+    for i in 1:(graphSize*5)
+        add_edge!(G, rand(1:graphSize), rand(1:graphSize), rand(0:10));
     end
-    return graph
-end;
-
-# Draw a graphWidth x graphWidth with connections defined by maze edges, saves as pictureName.png
-function drawEdges(mazeEdges, graphWidth::Int64, pictureName::String)
-    Drawing(10 * graphWidth, 10 * graphWidth, pictureName * ".png")
-    background("black")
-    sethue("purple")
-    for j in 0 : graphWidth - 1
-        for i in 0 : graphWidth - 1
-            rect((10 * i) + 1, (10 * j) + 1, 8, 8, :fill)
-        end
-    end
-
-    for edge in mazeEdges
-        source = edge.src
-        dest = edge.dst
-        sourceX = ((source - 1) % graphWidth)
-        sourceY = convert(Int64, ((source - 1) ÷ graphWidth))
-        destX = ((dest - 1) % graphWidth)
-        destY = convert(Int64, ((dest - 1) ÷ graphWidth))
-
-        rect((10 * sourceX) + 1, (10 * sourceY) + 1, 8 + (10 * (destX - sourceX)), 8 + (10 * (destY - sourceY)), :fill)
-    end
-
-    Luxor.finish()
-end;
+    return G;
+end
 
 #PARALLEL CODE
 
@@ -213,19 +163,21 @@ function seqPrims(G, numberOfNodes)
     return edges;
 end
 
-# MAIN
-graphWidth = 100;
-nodes = graphWidth^2;
-g = makeGrid(graphWidth, 10.0);
-gEdges = LightGraphs.edges(g);
-primsEdges = LightGraphs.prim_mst(g);
-seqPrim = seqPrims(g, nodes);
-paraPrim = parallelPrims(g, nodes);
-#drawEdges(paraPrim, graphWidth, "parallel");
-#drawEdges(primsEdges, graphWidth, "mst");
+#Create test graph
+nodes = 100;
+g = createTestGraph(nodes);
+#primsEdges = LightGraphs.prim_mst(g);
+#seqPrim = seqPrims(g, nodes);
+#paraPrim = parallelPrims(g, nodes);
 
+println("Number of Vertices: ", nodes)
+println("Base library's Prim's (seq):")
 @benchmark LightGraphs.prim_mst(g)
 
+println("Number of Vertices: ", nodes)
+println("Our implementation (seq):")
 @benchmark seqPrims(g, nodes)
 
+println("Number of Vertices: ", nodes)
+println("Our implementation (parallel):")
 @benchmark parallelPrims(g, nodes)
