b3fc7135cedf3aea88de19fb8209658df1a32f04
Aorthi <aorthi@hotmail.com>
2019-06-03 11:33:47 +1200

    Code clean up

--- PrimsAorthi/parallelPrims.jl
+++ PrimsAorthi/parallelPrims.jl
@@ -1,78 +1,18 @@
-# Comment these package adds in and out as needed
+#comment adding these packages in and out as needed
 #import Pkg;
-#Pkg.add("GraphPlot")
 #Pkg.add("LightGraphs")
-#Pkg.add("LightGraphs")
-#Pkg.add("Compose")
-#Pkg.add("Fontconfig")
-#Pkg.add("Cairo")
-#Pkg.add("Distributed")
-#Pkg.add("BenchmarkTools")
-#Pkg.add("SimpleTraits")
-#Pkg.add("DataStructures")
 #Pkg.add("SimpleWeightedGraphs")
-#Pkg.add("Luxor")
 
-using GraphPlot
 using LightGraphs
-import LightGraphs
-import LightGraphs.Parallel
-using Compose
-using Fontconfig
-using Cairo
-using Distributed
-using BenchmarkTools
-using SimpleTraits
-import DataStructures
-import SimpleWeightedGraphs
-using Luxor
-
-# Make a graphWidth x graphWidth grid with random weights on edges from 1 : maxWeight
-function makeGrid(graphWidth::Int64, maxWeight::Float64)
-    vertexAmount = graphWidth ^ 2
-    graph = SimpleWeightedGraphs.SimpleWeightedGraph(vertexAmount)
-    for i::Int64 in 1 : vertexAmount
-        if (i % graphWidth > 0)
-            LightGraphs.add_edge!(graph, i, i + 1, rand(Float64) * maxWeight)
-        end
-        if ((i - 1) / graphWidth < graphWidth - 1)
-            LightGraphs.add_edge!(graph, i, i + graphWidth, rand(Float64) * maxWeight)
-        end
-    end
-    return graph
-end;
-
-# Draw a graphWidth x graphWidth with connections defined by maze edges, saves as pictureName.png
-function drawEdges(mazeEdges, graphWidth::Int64, pictureName::String)
-    Drawing(10 * graphWidth, 10 * graphWidth, pictureName * ".png")
-    background("black")
-    sethue("purple")
-    for j in 0 : graphWidth - 1
-        for i in 0 : graphWidth - 1
-            rect((10 * i) + 1, (10 * j) + 1, 8, 8, :fill)
-        end
-    end
-
-    for edge in mazeEdges
-        source = edge.src
-        dest = edge.dst
-        sourceX = ((source - 1) % graphWidth)
-        sourceY = convert(Int64, ((source - 1) ÷ graphWidth))
-        destX = ((dest - 1) % graphWidth)
-        destY = convert(Int64, ((dest - 1) ÷ graphWidth))
-
-        rect((10 * sourceX) + 1, (10 * sourceY) + 1, 8 + (10 * (destX - sourceX)), 8 + (10 * (destY - sourceY)), :fill)
-    end
-
-    Luxor.finish()
-end;
+using SimpleWeightedGraphs
 
 # Finds the node with the lowest edge weight currently in key
-function parallelMinKey(key, visited, numberOfNodes)
-    # Creates atomic global variables
+function minKey(key, visited, numberOfNodes)
+    # Creates global variables
     globalMin = Inf;
     globalIndex = 1;
 
+    # Create a mutex
     m = Threads.Mutex();
 
     # Goes through each unvisited node and checks to see if its edge weight is small than the current local minimum weight
@@ -115,7 +55,7 @@ function parallelPrims(G, numberOfNodes)
 
     # Chooses which node to travel to based off what has the lowest value in key
     while 0 in visited
-        nextNode = parallelMinKey(key, visited, numberOfNodes);
+        nextNode = minKey(key, visited, numberOfNodes);
         visited[nextNode] = 1;
 
         # Updates the key array so that it contains the lowest weights of all possible paths
@@ -136,13 +76,3 @@ function parallelPrims(G, numberOfNodes)
     end
     return edges;
 end
-
-# MAIN
-graphWidth = 100;
-nodes = graphWidth^2;
-g = makeGrid(graphWidth, 10.0);
-gEdges = LightGraphs.edges(g);
-primsEdges = LightGraphs.prim_mst(g);
-paraPrim = parallelPrims(g, nodes);
-drawEdges(paraPrim, graphWidth, "parallel");
-drawEdges(primsEdges, graphWidth, "mst");
